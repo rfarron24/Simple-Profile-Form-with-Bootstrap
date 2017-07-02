77edbb0db74fa62a451b88a486ce14d2b6e731f1
rihaffarron <rihaf.husen@outlook.com>
2017-07-02 19:00:29 +0700

    Apply "Atom BEAUTIFY" index css mainJS citiesJS

---
+++ js/cities.js
@@ -0,0 +1,203 @@
+function city() {
+  var birthPlace = document.getElementById('birthplaceID');
+  var city = document.getElementById('selectCity');
+
+
+  if (document.getElementById('birthplaceID').value == 'Alexander') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Armand">Armand</option>' +
+      '<option  value="Bellevenue">Bellevenue</option>' +
+      '<option  value="Creste">Creste</option>' +
+      '<option  value="Great City of Alexander">Great City of Alexander</option>' +
+      '<option  value="Hermond">Hermond</option>' +
+      '<option  value="Ilrieth">Ilrieth</option>' +
+      '<option  value="Jeanarc">Jeanarc</option>' +
+      '<option  value="Livingwood">Livingwood</option>' +
+      '<option  value="Mistell Coast">Mistell Coast</option>' +
+      '<option  value="Vernecastell">Vernecastell</option>';
+
+  } else
+  if (document.getElementById('birthplaceID').value == 'Arkridge') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Arrano">Arrano</option>' +
+      '<option  value="Erwizi">Erwizi</option>' +
+      '<option  value="Dimarinski">Dimarinski</option>' +
+      '<option  value="Kaltrach">Kaltrach</option>' +
+      '<option  value="Letagin">Letagin</option>' +
+      '<option  value="Marunsk">Marunsk</option>' +
+      '<option  value="Mossiah">Mossiah</option>' +
+      '<option  value="Otso">Otso</option>' +
+      '<option  value="Suwedo">Suwedo</option>' +
+      '<option  value="Tarufa">Tarufa</option>';
+
+  } else
+  if (document.getElementById('birthplaceID').value == 'Azel') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Ellarach">Ellarach</option>' +
+      '<option  value="Dinger">Dinger</option>' +
+      '<option  value="Glidenarch">Glidenarch</option>' +
+      '<option  value="Merjole">Merjole</option>' +
+      '<option  value="Minks">Minks</option>' +
+      '<option  value="Wallen">Wallen</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Celes') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Abott">Abott</option>' +
+      '<option  value="Glidenarch">Bonnes</option>' +
+      '<option  value="Cyro">Cyro</option>' +
+      '<option  value="Hangar">Hangar</option>' +
+      '<option  value="Pillar">Pillar</option>' +
+      '<option  value="Tribute">Tribute</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Juno') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Albanuis">Albanuis</option>' +
+      '<option  value="Andelis">Andelis</option>' +
+      '<option  value="Crinita">Crinita</option>' +
+      '<option  value="Frey">Frey</option>' +
+      '<option  value="Krafan">Krafan</option>' +
+      '<option  value="Leune">Leune</option>' +
+      '<option  value="Simisel">Simisel</option>' +
+      '<option  value="Swansthem">Swansthem</option>' +
+      '<option  value="Vitatran">Vitatran</option>' +
+      '<option  value="Vlagga">Vlagga</option>' +
+      '<option  value="Zenaba">Zenaba</option>' +
+      '<option  value="Zenba">Zenba</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Latre') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Amberbrook">Amberbrook</option>' +
+      '<option  value="Delli">Delli</option>' +
+      '<option  value="Flitch">Flitch</option>' +
+      '<option  value="Sabiel">Sabiel</option>' +
+      '<option  value="Sulfan">Sulfan</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Midray') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Cathaway">Cathaway</option>' +
+      '<option  value="Fern">Fern</option>' +
+      '<option  value="Hirjait">Hirjait</option>' +
+      '<option  value="Ifliem">Ifliem</option>' +
+      '<option  value="Islegate">Islegate</option>' +
+      '<option  value="Kradstan">Kradstan</option>' +
+      '<option  value="Lebune">Lebune</option>' +
+      '<option  value="Mikhra">Mikhra</option>' +
+      '<option  value="Pinnari">Pinnari</option>' +
+      '<option  value="Tundra">Tundra</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Mosh') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Anvolli">Anvolli</option>' +
+      '<option  value="Bellmont">Bellmont</option>' +
+      '<option  value="Claymore">Claymore</option>' +
+      '<option  value="Hammersforth">Hammersforth</option>' +
+      '<option  value="Illuvias">Illuvias</option>' +
+      '<option  value="Lunarlute">Lunarlute</option>' +
+      '<option  value="Misisles">Misisles</option>' +
+      '<option  value="Oberch">Oberch</option>' +
+      '<option  value="Ophellum">Ophellum</option>' +
+      '<option  value="Ouluf">Ouluf</option>' +
+      '<option  value="Reverswode">Reverswode</option>' +
+      '<option  value="Rivelnieve">Rivelnieve</option>' +
+      '<option  value="Rootbeak">Rootbeak</option>' +
+      '<option  value="Segiberfrons">Segiberfrons</option>' +
+      '<option  value="Trident">Trident</option>' +
+      '<option  value="Tristan">Tristan</option>' +
+      '<option  value="Tusseas">Tusseas</option>' +
+      '<option  value="Volant">Volant</option>' +
+      '<option  value="Wildwood">Wildwood</option>' +
+      '<option  value="Zurreil">Zurreil</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Northern Mainands') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Canyon">Canyon</option>' +
+      '<option  value="Rock">Rock</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Terravine') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Havana">Havana</option>' +
+      '<option  value="Limbo">Limbo</option>' +
+      '<option  value="Luxor">Luxor</option>' +
+      '<option  value="Stardust">Stardust</option>' +
+      '<option  value="Tribune">Tribune</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Torsk') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Baille">Baille</option>' +
+      '<option  value="Cerma">Cerma</option>' +
+      '<option  value="Duscan">Duscan</option>' +
+      '<option  value="Fallhale">Fallhale</option>' +
+      '<option  value="Verena">Verena</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Veccel') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Minertown">Minertown</option>' +
+      '<option  value="Nautillus">Nautillus</option>' +
+      '<option  value="Nore">Nore</option>' +
+      '<option  value="Raven">Raven</option>' +
+      '<option  value="Rustville">Rustville</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'East Viea') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Atlas">Atlas</option>' +
+      '<option  value="Azuris">Azuris</option>' +
+      '<option  value="Chronos">Chronos</option>' +
+      '<option  value="Concept">Concept</option>' +
+      '<option  value="Cosmo">Cosmo</option>' +
+      '<option  value="Metroplex">Metroplex</option>' +
+      '<option  value="Neopolis">Neopolis</option>' +
+      '<option  value="Nimbus">Nimbus</option>' +
+      '<option  value="Pionix">Pionix</option>' +
+      '<option  value="Torens">Torens</option>' +
+      '<option  value="Torrus">Torrus</option>' +
+      '<option  value="Vendale">Vendale</option>' +
+      '<option  value="Vice">Vice</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'South Viea') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Donner Viea">Donner Viea</option>' +
+      '<option  value="Kresse">Kresse</option>' +
+      '<option  value="Rodbell">Rodbell</option>' +
+      '<option  value="Transmund">Transmund</option>' +
+      '<option  value="Tropa">Tropa</option>' +
+      '<option  value="Verdegriss">Verdegriss</option>' +
+      '<option  value="Vierra">Vierra</option>';
+  } else
+  if (document.getElementById('birthplaceID').value == 'Zathurn') {
+    document.getElementById('selectCity').innerHTML = '';
+    city.innerHTML +=
+      '<option selected disabled>Choose city...</option>' +
+      '<option  value="Duskbridge">Duskbridge</option>' +
+      '<option  value="Jotun">Jotun</option>' +
+      '<option  value="Rattlefang">Rattlefang</option>' +
+      '<option  value="Roiplean">Roiplean</option>';
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.getElementById('formEntry').addEventListener('submit', save);
 
-function save(e){
+function save(e) {
   var id = document.getElementById('idID').value;
   var name = document.getElementById('nameID').value;
   var sex = document.getElementById('sexID').value;
@@ -15,7 +15,7 @@ function save(e){
   var affiliation = document.getElementById('affiliationID').value;
   var activepost = document.getElementById('currentpostID').value;
 
-  if(!validateForm(id, name, sex, age, dob, weight, height, eye, hair, birthplace, cityorigin, affiliation, activepost)){
+  if (!validateForm(id, name, sex, age, dob, weight, height, eye, hair, birthplace, cityorigin, affiliation, activepost)) {
     return false;
   }
   var person = {
@@ -34,96 +34,95 @@ function save(e){
     activepostperson: activepost
   }
 
-  if (localStorage.getItem('ges') === null){
+  if (localStorage.getItem('ges') === null) {
     var ges = [];
     ges.push(person);
     localStorage.setItem('ges', JSON.stringify(ges));
-  }
-  else {
+  } else {
     var ges = JSON.parse(localStorage.getItem('ges'));
     ges.push(person);
     localStorage.setItem('ges', JSON.stringify(ges));
   }
-    document.getElementById('formEntry').reset();
-    fetchges();
-    e.preventDefault();
-  }
+  document.getElementById('formEntry').reset();
+  fetchges();
+  e.preventDefault();
+}
 
-function deleteprofile(id){
+function deleteprofile(id) {
   var ges = JSON.parse(localStorage.getItem('ges'));
-  alert("Are you sure want to delete "+id+" ?");
+  alert("Are you sure want to delete " + id + " ?");
 
-  for(var i=0; i < ges.length; i++){
-    if (ges[i].idperson == id){
-      ges.splice(i,1);
+  for (var i = 0; i < ges.length; i++) {
+    if (ges[i].idperson == id) {
+      ges.splice(i, 1);
     }
   }
   localStorage.setItem('ges', JSON.stringify(ges));
   fetchges();
 }
 
-  function fetchges(){
-    var ges = JSON.parse(localStorage.getItem('ges'));
-    var profilelist = document.getElementById('resultProfile');
+function fetchges() {
+  var ges = JSON.parse(localStorage.getItem('ges'));
+  var profilelist = document.getElementById('resultProfile');
 
-    profilelist.innerHTML = '';
+  profilelist.innerHTML = '';
 
-    for(var i=0; i<ges.length; i++){
+  for (var i = 0; i < ges.length; i++) {
 
-      var id = ges[i].idperson;
-      var name = ges[i].nameperson;
-      var sex = ges[i].sexperson;
-      var age = ges[i].ageperson;
-      var dob = ges[i].dobperson;
-      var weight = ges[i].weightperson;
-      var height = ges[i].heightperson;
-      var eye = ges[i].eyeperson;
-      var hair = ges[i].hairperson;
-      var birthplace = ges[i].birthplaceperson;
-      var cityorigin = ges[i].cityoriginperson;
-      var affiliation = ges[i].affiliationperson;
-      var activepost = ges[i].activepostperson;
+    var id = ges[i].idperson;
+    var name = ges[i].nameperson;
+    var sex = ges[i].sexperson;
+    var age = ges[i].ageperson;
+    var dob = ges[i].dobperson;
+    var weight = ges[i].weightperson;
+    var height = ges[i].heightperson;
+    var eye = ges[i].eyeperson;
+    var hair = ges[i].hairperson;
+    var birthplace = ges[i].birthplaceperson;
+    var cityorigin = ges[i].cityoriginperson;
+    var affiliation = ges[i].affiliationperson;
+    var activepost = ges[i].activepostperson;
 
-      profilelist.innerHTML += '<div class="well">' +
-                              '<label class="control-label col-sm-3">ID : </label>'+
-                              '<h4>'  + id + '</h4>' +
-                               '<label class="control-label col-sm-3">Name : </label>'+
-                               '<h4>' + name + '</h4>' +
-                               '<label class="control-label col-sm-3">Sex : </label>'+
-                               '<h4>' + sex + '</h4>' +
-                               '<label class="control-label col-sm-3">Age : </label>'+
-                               '<h4>' + age + '</h4>' +
-                               '<label class="control-label col-sm-3">Date of Birth : </label>'+
-                               '<h4>' + dob + '</h4>' +
-                               '<label class="control-label col-sm-3">Weight : </label>'+
-                               '<h4>' + weight + ' Kilograms </h4>' +
-                               '<label class="control-label col-sm-3">Weight : </label>'+
-                               '<h4>' + height + ' Meters </h4>' +
-                               '<label class="control-label col-sm-3">Eye Color : </label>'+
-                               '<h4>' + eye + '</h4>' +
-                               '<label class="control-label col-sm-3">Hair : </label>'+
-                               '<h4>' + hair + '</h4>' +
-                               '<label class="control-label col-sm-3">Origin : </label>'+
-                               '<h4>' + birthplace + '</h4>' +
-                               '<label class="control-label col-sm-3">Region : </label>'+
-                               '<h4>' + cityorigin + '</h4>' +
-                               '<label class="control-label col-sm-3">Official Affiliation : </label>'+
-                               '<h4>' + affiliation + '</h4>' +
-                               '<label class="control-label col-sm-4">Current Active Post : </label>'+
-                               '<h4>' + activepost + '</h4>' +
-                               '<a href="#" onclick="deleteprofile(\''+id+'\')" class="btn btn-danger" style="margin-left:3%;">Delete</a>' +
-                               '</div>';
+    profilelist.innerHTML += '<div class="well">' +
+      '<label class="control-label col-sm-3">ID : </label>' +
+      '<h4>' + id + '</h4>' +
+      '<label class="control-label col-sm-3">Name : </label>' +
+      '<h4>' + name + '</h4>' +
+      '<label class="control-label col-sm-3">Sex : </label>' +
+      '<h4>' + sex + '</h4>' +
+      '<label class="control-label col-sm-3">Age : </label>' +
+      '<h4>' + age + '</h4>' +
+      '<label class="control-label col-sm-3">Date of Birth : </label>' +
+      '<h4>' + dob + '</h4>' +
+      '<label class="control-label col-sm-3">Weight : </label>' +
+      '<h4>' + weight + ' Kilograms </h4>' +
+      '<label class="control-label col-sm-3">Weight : </label>' +
+      '<h4>' + height + ' Meters </h4>' +
+      '<label class="control-label col-sm-3">Eye Color : </label>' +
+      '<h4>' + eye + '</h4>' +
+      '<label class="control-label col-sm-3">Hair : </label>' +
+      '<h4>' + hair + '</h4>' +
+      '<label class="control-label col-sm-3">Origin : </label>' +
+      '<h4>' + birthplace + '</h4>' +
+      '<label class="control-label col-sm-3">Region : </label>' +
+      '<h4>' + cityorigin + '</h4>' +
+      '<label class="control-label col-sm-3">Official Affiliation : </label>' +
+      '<h4>' + affiliation + '</h4>' +
+      '<label class="control-label col-sm-4">Current Active Post : </label>' +
+      '<h4>' + activepost + '</h4>' +
+      '<a href="#" onclick="deleteprofile(\'' + id + '\')" class="btn btn-danger" style="margin-left:3%;">Delete</a>' +
+      '</div>';
 
-    }
   }
+}
 
-  function validateForm(id, name, sex, age, dob, weight, height, eye, hair, birthplace, cityorigin, affiliation, activepost){
+function validateForm(id, name, sex, age, dob, weight, height, eye, hair, birthplace, cityorigin, affiliation, activepost) {
 
-    if(!id || !name || !sex || !age || !dob ||
-      !weight || !height || !eye || !hair || !birthplace ||
-      !cityorigin || !affiliation || !activepost){
-      alert("Please complete the form first!");
-      return false;
-    }
-    return true;
+  if (!id || !name || !sex || !age || !dob ||
+    !weight || !height || !eye || !hair || !birthplace ||
+    !cityorigin || !affiliation || !activepost) {
+    alert("Please complete the form first!");
+    return false;
   }
+  return true;
+}
